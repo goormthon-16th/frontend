830e9106e5285c238f099e44dddb90b4df97a8c9
Park yejin <92977621+yezzan9@users.noreply.github.com>
2025-12-05 04:39:08 +0900

    feat: 디자인 변경사항 반영

--- src/components/detail/PostBlock.jsx
+++ src/components/detail/PostBlock.jsx
@@ -1,48 +1,48 @@
-import { Text, VStack, HStack, Badge } from "@vapor-ui/core";
+import { Text, VStack, HStack, Badge, Flex } from "@vapor-ui/core";
 import Image from "next/image";
 import "@/styles/font.css";
 
 const PostBlock = ({ isFirst, title, content }) => {
   return (
-    <VStack margin="$200 0" gap="$200">
-      <div
-        style={{
-          width: "100%",
-          position: "relative",
-          height: 0,
-          overflow: "hidden",
-          paddingTop: "70%",
-          borderRadius: "8px",
-        }}
-      >
-        <Image
-          fill
-          src="https://cdn.pixabay.com/photo/2025/09/12/16/49/dog-9830833_1280.jpg"
-          alt="img"
-          style={{ objectFit: "cover" }}
-          sizes="90vw"
-        />
-      </div>
-      <Text
-        style={{
-          fontFamily: "KCC Sonkeechung",
-          fontSize: "24px",
-          fontWeight: "500",
-          marginTop: "20px",
-        }}
-      >
-        {title}
-      </Text>
-      {isFirst && (
-        <HStack gap="$100">
-          {["감성", "이주민", "귤칵테일"].map((tag) => {
-            return <Badge key={tag}>{tag}</Badge>;
-          })}
-        </HStack>
+    <>
+      {!isFirst && (
+        <Flex margin="$400 0">
+          <Text style={{ fontSize: "14px", lineHeight: 1.5 }}>{content}</Text>
+        </Flex>
       )}
+      <VStack margin="$200 0" gap="$200">
+        <div
+          style={{
+            width: "100%",
+            position: "relative",
+            height: 0,
+            overflow: "hidden",
+            paddingTop: "70%",
+            borderRadius: "8px",
+          }}
+        >
+          <Image
+            fill
+            src="https://cdn.pixabay.com/photo/2025/09/12/16/49/dog-9830833_1280.jpg"
+            alt="img"
+            style={{ objectFit: "cover" }}
+            sizes="90vw"
+          />
+        </div>
+        <Text
+          style={{
+            fontFamily: "KCC Sonkeechung",
+            fontSize: "24px",
+            fontWeight: "500",
+            marginTop: "20px",
+          }}
+        >
+          {title}
+        </Text>
 
-      <Text style={{ fontSize: "14px", lineHeight: 1.5, marginTop: "10px" }}>{content}</Text>
-    </VStack>
+        <Text style={{ fontSize: "14px", lineHeight: 1.5, marginTop: "10px" }}>{content}</Text>
+      </VStack>
+    </>
   );
 };
 
