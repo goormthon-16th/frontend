d0834373d91f61645bf34d19f36ca298a46f525a
SuJin <kimsujin001127@gmail.com>
2025-12-05 04:33:41 +0900

    feat: 홈 스와이퍼 구현

--- src/components/template/home/HomeTemplate.jsx
+++ src/components/template/home/HomeTemplate.jsx
@@ -1,9 +1,15 @@
+import MainSwiper from "@/components/MainSwiper";
 import SpotSwiper from "@/components/SpotSwiper";
+import { Text } from "@vapor-ui/core";
 import React from "react";
 
 export const HomeTemplate = () => {
   return (
     <div>
+      <MainSwiper />
+      <Text typography="heading6" className="text-gray-500">
+        다른 사장님들의 이야기에요
+      </Text>
       <SpotSwiper />
     </div>
   );
